2d77f7c65bbebdc227b4691afae1e34e28cf2bab
ing mariano ramos <ing mariano ramos@LAPTOP-U9LBT6JE>
2019-03-31 16:27:08 -0400

    proyecto de fausto

---
+++ JavaApplication27/src/javaapplication27/JavaApplication27.java
@@ -0,0 +1,21 @@
+/*ing mariano ramos 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication27;
+
+/**
+ *
+ * @author ing mariano ramos
+ */
+public class JavaApplication27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
